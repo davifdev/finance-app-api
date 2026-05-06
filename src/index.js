a8b6ed018aba92e864e59ec40b2ec0e0bcf65740
Davi Fernandes <davi.fer159@gmail.com>
2026-05-06 09:18:11 -0300

    feat: add PostgresCreateUserRepository

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 import express from "express";
-import { PostgresHelper } from "./db/postgres/client.js";
 
 const app = express();
-const PORT = 3000;
 
-app.get("/", async (req, res) => {
-  const results = await PostgresHelper.query("SELECT * FROM users;");
-  res.send(JSON.stringify(results));
-});
+app.use(express.json());
+const PORT = 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost${PORT}`);
