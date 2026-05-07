f758ed815423ba7981d94241deb1ea372ca73e8f
Davi Fernandes <davi.fer159@gmail.com>
2026-05-07 07:59:20 -0300

    feat: add GetUserByIdController and validations

--- src/repositories/postgres/get-user-by-id.js
+++ src/repositories/postgres/get-user-by-id.js
@@ -1,9 +1,10 @@
 import { PostgresHelper } from "../../db/postgres/client.js";
 export class PostgresGetUserByIdRepository {
   async execute(userId) {
-    const user = PostgresHelper.query("SELECT * FROM users WHERE id = $1", [
-      userId,
-    ]);
+    const user = await PostgresHelper.query(
+      "SELECT * FROM users WHERE id = $1",
+      [userId],
+    );
 
     return user[0];
   }
