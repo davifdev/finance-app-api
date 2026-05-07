d7dd70a4c64f648b8bf92769b6202c76aa949957
Davi Fernandes <davi.fer159@gmail.com>
2026-05-07 07:31:17 -0300

    feat: add PostgresGetUserByIdRepository

---
+++ src/repositories/postgres/get-user-by-id.js
@@ -0,0 +1,10 @@
+import { PostgresHelper } from "../../db/postgres/client.js";
+export class PostgresGetUserByIdRepository {
+  async execute(userId) {
+    const user = PostgresHelper.query("SELECT * FROM users WHERE id = $1", [
+      userId,
+    ]);
+
+    return user[0];
+  }
+}
