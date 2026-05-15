59cf33ee560fed54bd2114aec087bc25dd0b4253
Davi Fernandes <davi.fer159@gmail.com>
2026-05-15 13:53:52 -0300

    feat: ensure DeleteUser and GetUserByIdRepository use prisma

--- src/repositories/postgres/user/get-user-by-id.js
+++ src/repositories/postgres/user/get-user-by-id.js
@@ -1,11 +1,19 @@
-import { PostgresHelper } from "../../../db/postgres/client.js";
+// import { PostgresHelper } from "../../../db/postgres/client.js";
+import { prisma } from "../../../../prisma/prisma.js";
 export class PostgresGetUserByIdRepository {
-  async execute(userId) {
-    const user = await PostgresHelper.query(
-      "SELECT * FROM users WHERE id = $1",
-      [userId],
-    );
+  // async execute(userId) {
+  //   const user = await PostgresHelper.query(
+  //     "SELECT * FROM users WHERE id = $1",
+  //     [userId],
+  //   );
+  //   return user[0];
+  // }
 
-    return user[0];
+  async execute(userId) {
+    return await prisma.user.findUnique({
+      where: {
+        id: userId,
+      },
+    });
   }
 }
