dfffed00bdab5f0b2dafa451fac594506aeea874
Davi Fernandes <davi.fer159@gmail.com>
2026-05-18 09:32:30 -0300

    test: ensure 400 is returned if first_name is not provided

--- src/controllers/user/create-user.test.js
+++ src/controllers/user/create-user.test.js
@@ -28,4 +28,20 @@ describe("CreateUserController", () => {
     expect(result.statusCode).toBe(201);
     expect(result.body).not.toBeNull();
   });
+
+  it("should return 400 if first_name is not provided", async () => {
+    const createUserUseCase = new CreateUserUseCaseStub();
+    const createUserController = new CreateUserController(createUserUseCase);
+
+    const httpRequest = {
+      body: {
+        last_name: "Fernandes",
+        email: "davi@gmail.com",
+        password: "123456",
+      },
+    };
+
+    const result = await createUserController.execute(httpRequest);
+    expect(result.statusCode).toBe(400);
+  });
 });
