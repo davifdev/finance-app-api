b2833309ecb6a4278d1411642bccdffdb9668df8
Davi Fernandes <davi.fer159@gmail.com>
2026-05-18 09:03:59 -0300

    test: ensure 201 is returned when user is created

---
+++ src/controllers/user/create-user.test.js
@@ -0,0 +1,31 @@
+import { CreateUserController } from "./create-user.js";
+
+describe("CreateUserController", () => {
+  class CreateUserUseCaseStub {
+    execute(user) {
+      return user;
+    }
+  }
+
+  it("should return 201 when creating a user successfuly", async () => {
+    // Arrange
+    const createUserUseCase = new CreateUserUseCaseStub();
+    const createUserController = new CreateUserController(createUserUseCase);
+
+    const httpRequest = {
+      body: {
+        first_name: "Davi",
+        last_name: "Fernandes",
+        email: "davi@gmail.com",
+        password: "123456",
+      },
+    };
+
+    // Act
+    const result = await createUserController.execute(httpRequest);
+
+    // Assert
+    expect(result.statusCode).toBe(201);
+    expect(result.body).not.toBeNull();
+  });
+});
