2d310fed3af38a1a5ac00bce42772d300238330c
Davi Fernandes <davi.fer159@gmail.com>
2026-05-19 18:39:30 -0300

    test: ensure DeleteUserController returns 404 if user not found

--- src/controllers/user/delete-user.test.js
+++ src/controllers/user/delete-user.test.js
@@ -50,4 +50,14 @@ describe("DeleteUserController", () => {
 
     expect(result.statusCode).toBe(400);
   });
+
+  it("should return 404 if user is not found", async () => {
+    const { sut, deleteUserUseCase } = makeSut();
+
+    jest.spyOn(deleteUserUseCase, "execute").mockResolvedValue(null);
+
+    const result = await sut.execute(httpRequest);
+
+    expect(result.statusCode).toBe(404);
+  });
 });
