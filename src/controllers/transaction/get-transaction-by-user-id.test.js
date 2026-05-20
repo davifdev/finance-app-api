be7bd32848b854be56879371dac90152f3b60778
Davi Fernandes <davi.fer159@gmail.com>
2026-05-20 12:46:55 -0300

    test: ensure GetTransactionByUserId returns 200 if tranasaction are retrieved

---
+++ src/controllers/transaction/get-transaction-by-user-id.test.js
@@ -0,0 +1,34 @@
+import { faker } from "@faker-js/faker";
+import { GetTransactionByUserIdController } from "./get-transaction-by-user-id";
+
+describe("GetTransactionByUserIdController", () => {
+  const makeSut = () => {
+    class GetTransactionByUserIdUseCaseStub {
+      async execute(transaction) {
+        return transaction;
+      }
+    }
+
+    const getTransactionByUserIdUseCase =
+      new GetTransactionByUserIdUseCaseStub();
+    const sut = new GetTransactionByUserIdController(
+      getTransactionByUserIdUseCase,
+    );
+
+    return { getTransactionByUserIdUseCase, sut };
+  };
+
+  const httpRequest = {
+    query: {
+      userId: faker.string.uuid(),
+    },
+  };
+
+  it("should return 200 if transactions are retrieved", async () => {
+    const { sut } = makeSut();
+
+    const result = await sut.execute(httpRequest);
+
+    expect(result.statusCode).toBe(200);
+  });
+});
