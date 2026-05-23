909964241d94f804ec49f4248b0c03d777ee3adb
Davi Fernandes <davi.fer159@gmail.com>
2026-05-23 12:27:22 -0300

    test: ensure GetUserBalanceUseCase throws if GetUserBalance throws

--- src/use-cases/user/get-user-balance.test.js
+++ src/use-cases/user/get-user-balance.test.js
@@ -97,7 +97,19 @@ describe("GetUserBalance", () => {
     const { sut, getUserByIdRepository } = makeSut();
 
     jest.spyOn(getUserByIdRepository, "execute").mockImplementation(() => {
-      new Error();
+      throw new Error();
+    });
+
+    const promise = sut.execute(faker.string.uuid());
+
+    await expect(promise).rejects.toThrow();
+  });
+
+  it("should throw if GetUserBalance throws", async () => {
+    const { sut, getUserBalanceRepository } = makeSut();
+
+    jest.spyOn(getUserBalanceRepository, "execute").mockImplementation(() => {
+      throw new Error();
     });
 
     const promise = sut.execute(faker.string.uuid());
