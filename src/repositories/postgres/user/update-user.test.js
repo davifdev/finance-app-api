2b6a5b24e4231fe2a8a0dad11947ec8640bdc692
Davi Fernandes <davi.fer159@gmail.com>
2026-05-26 15:24:45 -0300

    test: ensure UpdareUserRepositoru call prisma with correct params

--- src/repositories/postgres/user/update-user.test.js
+++ src/repositories/postgres/user/update-user.test.js
@@ -10,20 +10,36 @@ describe("UpdateUserRepository", () => {
     return { sut };
   };
 
+  const updateUserParams = {
+    id: faker.string.uuid(),
+    first_name: faker.person.firstName(),
+    last_name: faker.person.lastName(),
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+  };
+
   it("should update user on db", async () => {
     const user = await prisma.user.create({ data: fixturesUser });
     const { sut } = makeSut();
 
-    const updateUserParams = {
-      id: faker.string.uuid(),
-      first_name: faker.person.firstName(),
-      last_name: faker.person.lastName(),
-      email: faker.internet.email(),
-      password: faker.internet.password(),
-    };
-
     const result = await sut.execute(user.id, updateUserParams);
 
     expect(result).toStrictEqual(updateUserParams);
+  });
+
+  it("should call Prisma with correct params", async () => {
+    const user = await prisma.user.create({ data: fixturesUser });
+    const { sut } = makeSut();
+
+    const prismaSpy = jest.spyOn(prisma.user, "update");
+
+    await sut.execute(user.id, updateUserParams);
+
+    expect(prismaSpy).toHaveBeenCalledWith({
+      where: {
+        id: user.id,
+      },
+      data: updateUserParams,
+    });
   });
 });
