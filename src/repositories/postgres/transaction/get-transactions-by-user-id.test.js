83217dc7f8af1e69231e590466f19ff248214859
Davi Fernandes <davi.fer159@gmail.com>
2026-05-26 18:30:46 -0300

    test: ensure GetTransactionsByUserId get a transacion by user id on db

---
+++ src/repositories/postgres/transaction/get-transactions-by-user-id.test.js
@@ -0,0 +1,43 @@
+import { faker } from "@faker-js/faker";
+import { prisma } from "../../../../prisma/prisma";
+import { user } from "../../../__tests__";
+import { PostgresGetTransactionByUserIdRepository } from "./get-transactions-by-user-id";
+import dayjs from "dayjs";
+
+describe("GetTransactionsByUserId", () => {
+  const makeSut = () => {
+    const sut = new PostgresGetTransactionByUserIdRepository();
+
+    return { sut };
+  };
+
+  const transaction = {
+    id: faker.string.uuid(),
+    user_id: faker.string.uuid(),
+    name: faker.lorem.words(6),
+    date: faker.date.past().toISOString(),
+    type: "EARNING",
+    amount: faker.number.int(10),
+  };
+
+  it("should get a transaction by user id on db", async () => {
+    const { sut } = makeSut();
+    await prisma.user.create({ data: user });
+    await prisma.transaction.create({
+      data: { ...transaction, user_id: user.id },
+    });
+
+    const result = await sut.execute(user.id);
+
+    expect(result[0].id).toBe(transaction.id);
+    expect(result[0].user_id).toBe(user.id);
+    expect(result[0].name).toBe(transaction.name);
+    expect(result[0].type).toBe(transaction.type);
+    expect(result[0].amount.toString()).toBe(transaction.amount.toString());
+    expect(dayjs(result[0].date).daysInMonth()).toBe(
+      dayjs(transaction.date).daysInMonth(),
+    );
+    expect(dayjs(result[0].date).month()).toBe(dayjs(result[0].date).month());
+    expect(dayjs(result[0].date).year()).toBe(dayjs(result[0].date).year());
+  });
+});
