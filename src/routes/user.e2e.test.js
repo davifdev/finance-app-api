ed2d4735214631914876b58259df9c7f20a26fd8
Davi Fernandes <davi.fer159@gmail.com>
2026-05-27 13:06:05 -0300

    test: ensure /api/users/:userId/balance returns 200 and correct balance

--- src/routes/user.e2e.test.js
+++ src/routes/user.e2e.test.js
@@ -2,6 +2,7 @@ import request from "supertest";
 import { app } from "../index.js";
 import { user } from "../__tests__/index.js";
 import { faker } from "@faker-js/faker";
+import { TransactionType } from "../../generated/prisma/index.js";
 
 describe("User Routes E2E Tests", () => {
   it("POST /api/users should return 201 when user is created", async () => {
@@ -66,5 +67,56 @@ describe("User Routes E2E Tests", () => {
     const response = await request(app).delete(`/api/users/${createdUser.id}`);
 
     expect(response.status).toBe(200);
+  });
+
+  it("GET /api/users/:userId/balance returns 200 and correct balance", async () => {
+    const { body: createdUser } = await request(app)
+      .post("/api/users")
+      .send({
+        ...user,
+        id: undefined,
+      });
+
+    await request(app)
+      .post("/api/transactions")
+      .send({
+        user_id: createdUser.id,
+        name: faker.lorem.words(6),
+        date: "2026-05-19T18:30:00Z",
+        amount: 8000,
+        type: TransactionType.EARNING,
+      });
+
+    await request(app)
+      .post("/api/transactions")
+      .send({
+        user_id: createdUser.id,
+        name: faker.lorem.words(6),
+        date: "2026-05-19T18:30:00Z",
+        amount: 3000,
+        type: TransactionType.EXPENSE,
+      });
+
+    await request(app)
+      .post("/api/transactions")
+      .send({
+        user_id: createdUser.id,
+        name: faker.lorem.words(6),
+        date: "2026-05-19T18:30:00Z",
+        amount: 2000,
+        type: TransactionType.INVESTMENT,
+      });
+
+    const response = await request(app).get(
+      `/api/users/${createdUser.id}/balance`,
+    );
+
+    expect(response.status).toBe(200);
+    expect(response.body).toStrictEqual({
+      earnings: "8000",
+      expense: "3000",
+      investment: "2000",
+      balance: "3000",
+    });
   });
 });
