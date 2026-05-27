d6e528d70975b8b69d0b400c7d13ba5571f13adc
Davi Fernandes <davi.fer159@gmail.com>
2026-05-26 21:21:47 -0300

    test: ensure makeCreateUserController return a valid CreateUserController

--- src/factories/controllers/user.test.js
+++ src/factories/controllers/user.test.js
@@ -1,8 +1,12 @@
-import { GetUserByIdController } from "../../controllers";
-import { makeGetUserByIdController } from "./user";
+import { CreateUserController, GetUserByIdController } from "../../controllers";
+import { makeCreateUserController, makeGetUserByIdController } from "./user";
 
 describe("UserControllerFactories", () => {
   it("should return a valid GetUserByIdController instance", () => {
     expect(makeGetUserByIdController()).toBeInstanceOf(GetUserByIdController);
+  });
+
+  it("should return a valid CreateUserController instance", () => {
+    expect(makeCreateUserController()).toBeInstanceOf(CreateUserController);
   });
 });
