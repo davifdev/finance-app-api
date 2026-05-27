9e076e457e7efe0c0ba533df6c50ef25e4ad8d01
Davi Fernandes <davi.fer159@gmail.com>
2026-05-27 17:46:55 -0300

    test: ensure POST /api/transactions should return 201 when creating a transaction successfully

---
+++ src/routes/transaction.e2e.test.js
@@ -0,0 +1,25 @@
+import request from "supertest";
+import { app } from "../index.js";
+import { user } from "../__tests__/fixtures/user.js";
+
+describe("Transaction Routes E2E Test", () => {
+  it("POST /api/transactions should return 201 when creating a transaction successfully", async () => {
+    const { body: createdUser } = await request(app)
+      .post("/api/users")
+      .send({
+        ...user,
+        id: undefined,
+      });
+
+    const response = await request(app).post("/api/transactions").send({
+      name: "Salário",
+      user_id: createdUser.id,
+      date: "2026-05-19T18:30:00Z",
+      amount: 10.5,
+      type: "EARNING",
+    });
+
+    expect(response.status).toBe(201);
+    expect(response.body.user_id).toBe(createdUser.id);
+  });
+});
