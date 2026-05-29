8a8d37980452a1a90650e1a6b7f4c160f0d0188b
Davi Fernandes <davi.fer159@gmail.com>
2026-05-29 13:53:18 -0300

    ci: test ci

--- jest.global-setup.mjs
+++ jest.global-setup.mjs
@@ -2,7 +2,7 @@ import { execSync } from "child_process";
 import "dotenv/config";
 
 export default async function globalSetup() {
-  execSync("docker compose --env-file .env-test up -d --wait postgres-test", {
+  execSync("docker compose --env-file .env.test up -d --wait postgres-test", {
     stdio: "inherit",
   });
   execSync("dotenv -e .env.test -- npx prisma db push");
