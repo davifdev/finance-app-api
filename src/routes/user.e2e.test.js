44431b1cb7b87c16dd1366aba7a5af6d5dc31866
Davi Fernandes <davi.fer159@gmail.com>
2026-06-02 13:35:09 -0300

    test: add login user route

--- src/routes/user.e2e.test.js
+++ src/routes/user.e2e.test.js
@@ -162,4 +162,20 @@ describe("User Routes E2E Tests", () => {
 
     expect(response.status).toBe(400);
   });
+
+  it("POST /api/users/login should return 200 and tokens when user crendential are valid", async () => {
+    const { body: createdUser } = await request(app)
+      .post("/api/users")
+      .send({
+        ...user,
+        id: undefined,
+      });
+
+    const response = await request(app).post("/api/users/login").send({
+      email: createdUser.email,
+      password: user.password,
+    });
+
+    expect(response.status).toBe(200);
+  });
 });
